3fed01eb8bec256ba95ed755235db9a233863a8a
sugitayuuki <sugitayuuki@9DF7>
2023-09-13 22:33:29 +0900

    プロジェクト ファイルを追加します。

---
+++ ComboBoxWithImageSample/ComboBoxWithImage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ComboBoxWithImageSample
+{
+    public class ComboBoxWithImage : ComboBox
+    {
+
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageProperty =
+            DependencyProperty.Register("Image", typeof(ImageSource), typeof(ComboBoxWithImage), new PropertyMetadata(null));
+
+
+
+    }
+}
